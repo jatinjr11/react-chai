fcb5d16d958945c3366bf962024b186287bf482f
Jatin <jatin333x@gmail.com>
2025-05-10 11:02:42 +0530

    init-upt-1

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import ME from "./me.jsx"
+function App() {
+
+  return (
+    <>
+    <ME/> 
+    <h1>Ram Ram with Vite</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/me.jsx
@@ -0,0 +1,12 @@
+function ME(){
+    return (
+        <>
+        <div>Me hu</div>
+        <>{2+4}</>
+        <br></br>
+        <>{2+8}</>
+        </>
+    )
+}
+
+export default ME;
